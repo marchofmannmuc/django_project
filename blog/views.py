0aa79bb91f804e34615a39c89c9985260b9ed52f
marchofmannmuc <m.hofmann@hm.edu>
2019-02-20 23:34:40 +0100

    Grundlegende Funktionalität bezüglich Posts erstellt.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from datetime import datetime
+from .models import Post
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+
+   context = {
+       'posts': Post.objects.all()
+   }
+   return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About Page'})
+
