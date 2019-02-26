e3ae1b582ab38979bd77bbb1828e06c0a8f3269c
marchofmannmuc <m.hofmann@hm.edu>
2019-02-27 00:42:28 +0100

    Registrierung mit Nutername, Email und Passwort möglich.
    Registrierung mit Crispy Form verschönert.

--- users/views.py
+++ users/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
+
 # Create your views here.
 
 def register(request):
-    form = UserCreationForm()
+
+    if request.method == 'POST':
+         form = UserRegisterForm(request.POST)
+         if form.is_valid():
+             username = form.cleaned_data.get('Username')
+             messages.success(request, f'Account created for {username}!')
+             form.save()
+             return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
